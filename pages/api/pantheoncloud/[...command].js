88422b9af42c6c2a37af57788196479e1fc5444c
Roland Benedetti <rolandbenedetti@n0fcjxhfwg-rolandbenedetti.myfiosgateway.com>
2024-03-06 13:27:22 -0500

    first commit

---
+++ pages/api/pantheoncloud/[...command].js
@@ -0,0 +1,17 @@
+import { PantheonAPI, PantheonClient } from "@pantheon-systems/pcc-react-sdk";
+import { serverSmartComponentMap } from "../../../components/smart-components";
+
+export const pantheonAPIOptions = {
+  getPantheonClient: (props) =>
+    new PantheonClient({
+      pccHost: process.env.PCC_HOST,
+      siteId: process.env.PCC_SITE_ID,
+      apiKey: process.env.PCC_TOKEN,
+      ...props,
+    }),
+  resolvePath: (article) => `/articles/${article.slug || article.id}`,
+  getSiteId: () => process.env.PCC_SITE_ID,
+  smartComponentMap: serverSmartComponentMap,
+};
+
+export default PantheonAPI(pantheonAPIOptions);
